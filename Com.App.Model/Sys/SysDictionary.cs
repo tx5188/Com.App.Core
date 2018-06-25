d4e91f9e88909561dfe5b55b1994483efeeb321c
tangxiang <tx5188@aliyun.com>
2018-06-25 10:28:52 +0800

    2

--- Com.App.Model/Sys/SysDictionary.cs
+++ Com.App.Model/Sys/SysDictionary.cs
@@ -5,19 +5,48 @@ using System.Text;
 
 namespace Com.App.Model.Sys
 {
-    [Table(public string SYS_DICTIONARYpublic string )]
+    [Table("SYS_DICTIONARY")]
     public class SysDictionary : Entity
     {
-      
+      /// <summary>
+      /// 参数类型
+      /// </summary>
         public string DicType { get; set; }
+        /// <summary>
+        /// 编码
+        /// </summary>
         public string DicCode { get; set; }
+        /// <summary>
+        /// 参数名称
+        /// </summary>
         public string DicName { get; set; }
+        /// <summary>
+        /// 排序
+        /// </summary>
         public int DicOrder { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
         public string DicMemo{ get; set; }
+        /// <summary>
+        /// 删除标志 0 正常 1删除
+        /// </summary>
         public int DeleteFlag { get; set; }
-        public string RecordName { get; set; }
+        /// <summary>
+        /// 新增人
+        /// </summary>
+        public string RecordMan { get; set; }
+        /// <summary>
+        /// 新增日期
+        /// </summary>
         public DateTime RecordDate { get; set; }
-        public string UpdateName { get; set; }
+        /// <summary>
+        /// 更新人
+        /// </summary>
+        public string UpdateMan { get; set; }
+        /// <summary>
+        /// 更新日期
+        /// </summary>
         public DateTime UpdateDate { get; set; }
     }
 }
